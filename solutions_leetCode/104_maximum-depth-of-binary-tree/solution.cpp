e6587e905278433b185fa0879ac9518e51bbecdd
PacemakerX <sparsh.officialwork@gmail.com>
2024-12-26 04:53:25 +0530

    Adds one more question on depth

---
+++ solutions_leetCode/104_maximum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/maximum-depth-of-binary-tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int depth=0;
+    int maxDepth(TreeNode* root) {        
+
+        if(root==nullptr)
+            return 0;
+
+        depth=0;
+        Traversal(root,0);
+        return depth+1;
+    }
+
+    void Traversal(TreeNode* node, int level){
+        if(node==nullptr)
+            return;
+
+        if(node->left!=nullptr)
+            Traversal(node->left,level+1);
+        
+        if(node->right!=nullptr)
+            Traversal(node->right,level+1);
+        
+        depth=max(depth,level);
+    }
+};
